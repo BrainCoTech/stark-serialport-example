56b2117d20f203a98fe90bc455a18d83d4a4124f
Yongle <yong.work@gmail.com>
2024-08-14 18:03:24 +0800

    fix: remove `broadcast_enable` param for pymodbus==3.7.0 compatibility chore: add Python Black formatter

--- python/modbus_example/modbus_client_utils.py
+++ python/modbus_example/modbus_client_utils.py
@@ -1,6 +1,7 @@
 import sys
 import pathlib
 from pymodbus.utilities import hexlify_packets
+
 try:
     if sys.version_info.major == 3 and sys.version_info.minor >= 8:
         # FIXME: pymodbus 3.5.8 NOT support in python 3.7.x
@@ -16,21 +17,29 @@ except ImportError as e:
 current_dir = pathlib.Path(__file__).resolve()
 parent_dir = current_dir.parent.parent
 sys.path.append(str(parent_dir))
-sys.path.append(parent_dir.joinpath('protobuf_example'))
+sys.path.append(parent_dir.joinpath("protobuf_example"))
 from lib import *
-from protobuf_example.serial_utils import *     
+from protobuf_example.serial_utils import *
+
 
 def is_connected(client: ModbusSerialClient):
     if hasattr(client, "connected"):
         return getattr(client, "connected")
     else:
-        return client.connect()       
+        return client.connect()
 
-def client_connect(port: str, baudrate: BaudRate, broadcast_enable=False) -> ModbusSerialClient:
-    client = ModbusSerialClient(port=port, baudrate=baudrate.value, broadcast_enable=broadcast_enable, timeout=3)
+
+def client_connect(
+    port: str, baudrate: BaudRate, broadcast_enable: bool = False
+) -> ModbusSerialClient:
+    client = ModbusSerialClient(port=port, baudrate=baudrate.value, timeout=3)
+    # pymodbus==3.7.0, broadcast_enable parameters removed. (https://github.com/pymodbus-dev/pymodbus/pull/2272/files)
     if not is_connected(client):
-        raise ValueError(f"Failed to connect to Modbus client on port {port} at baudrate {baudrate}")
+        raise ValueError(
+            f"Failed to connect to Modbus client on port {port} at baudrate {baudrate}"
+        )
     return client
+
 
 def client_close(client: ModbusSerialClient) -> None:
     if isinstance(client, ModbusSerialClient):
@@ -39,13 +48,14 @@ def client_close(client: ModbusSerialClient) -> None:
         except Exception as e:
             SKLog.info(f"Error closing client: {e}")
 
+
 def client_write_data(client: ModbusSerialClient, data: bytes) -> int:
     SKLog.debug(f"client_write_data, {len(data)} bytes, {hexlify_packets(data)}")
     # SKLog.debug(f"client_write_data, {len(data)} bytes, {hexlify_packets_without_0x(data)}")
-    
+
     if isinstance(client, ModbusSerialClient) and is_connected(client):
         try:
-            ret = client.send(data) # Cleanup recv buffer before send
+            ret = client.send(data)  # Cleanup recv buffer before send
             # ret = client.socket.write(data)
             SKLog.debug(f"client_write_data, ret: {ret}")
             return 0
@@ -53,53 +63,78 @@ def client_write_data(client: ModbusSerialClient, data: bytes) -> int:
             SKLog.error(f"Error writing data: {e}")
     else:
         SKLog.error("client_write_data, client is not connected")
-    return -1            
+    return -1
 
-def client_write_registers(client: ModbusSerialClient, address: int, values: list, slave: int)-> int:
-    SKLog.debug(f"client_write_registers, address: {address}, len={len(values)}, {hexlify_packets(values)}")
+
+def client_write_registers(
+    client: ModbusSerialClient, address: int, values: list, slave: int
+) -> int:
+    SKLog.debug(
+        f"client_write_registers, address: {address}, len={len(values)}, {hexlify_packets(values)}"
+    )
     # SKLog.debug(f"client_write_registers, address: {address}, len={len(values)}, {hexlify_packets_without_0x(values)}")
-    
+
     if isinstance(client, ModbusSerialClient) and is_connected(client):
         try:
-            if isinstance(values, list): 
+            if isinstance(values, list):
                 if len(values) == 1:
                     client.write_register(address=address, value=values[0], slave=slave)
                     # time.sleep(0.1)
-                else:   
+                else:
                     client.write_registers(address=address, values=values, slave=slave)
             else:
-                SKLog.warning(f"client_write_registers, values type error: {type(values)}")     
-            return 0    
+                SKLog.warning(
+                    f"client_write_registers, values type error: {type(values)}"
+                )
+            return 0
         except Exception as e:
             SKLog.error(f"Error writing registers: {e}")
     else:
-        SKLog.error("client_write_registers, client is not connected")     
-    return -1         
+        SKLog.error("client_write_registers, client is not connected")
+    return -1
 
-def client_read_holding_registers(client: ModbusSerialClient, register_address: int, count: int = 1, slave: int = 0) -> list:
-    SKLog.debug(f"read_holding_registers, register_address: {register_address}, count: {count}")
+
+def client_read_holding_registers(
+    client: ModbusSerialClient, register_address: int, count: int = 1, slave: int = 0
+) -> list:
+    SKLog.debug(
+        f"read_holding_registers, register_address: {register_address}, count: {count}"
+    )
 
     if isinstance(client, ModbusSerialClient) and is_connected(client):
         try:
-            response = client.read_holding_registers(register_address, count=count, slave=slave)
+            response = client.read_holding_registers(
+                register_address, count=count, slave=slave
+            )
             values = response.registers
-            SKLog.debug(f"read_holding_registers, register_address: {register_address}, len={len(values)}, {hexlify_packets(values)}")
+            SKLog.debug(
+                f"read_holding_registers, register_address: {register_address}, len={len(values)}, {hexlify_packets(values)}"
+            )
             return values
         except Exception as e:
             SKLog.error(f"Error reading holding registers: {e}")
     else:
         SKLog.error("read_holding_registers, client is not connected")
 
-def client_read_input_registers(client: ModbusSerialClient, register_address: int, count: int = 1, slave: int = 0) -> list:      
-    SKLog.debug(f"read_input_registers, register_address: {register_address}, count: {count}")
+
+def client_read_input_registers(
+    client: ModbusSerialClient, register_address: int, count: int = 1, slave: int = 0
+) -> list:
+    SKLog.debug(
+        f"read_input_registers, register_address: {register_address}, count: {count}"
+    )
 
     if isinstance(client, ModbusSerialClient) and is_connected(client):
         try:
-            response = client.read_input_registers(register_address, count=count, slave=slave)
+            response = client.read_input_registers(
+                register_address, count=count, slave=slave
+            )
             values = response.registers
-            SKLog.debug(f"read_input_registers, register_address: {register_address}, len={len(values)}, {hexlify_packets(values)}")
+            SKLog.debug(
+                f"read_input_registers, register_address: {register_address}, len={len(values)}, {hexlify_packets(values)}"
+            )
             return values
         except Exception as e:
             SKLog.error(f"Error reading input registers: {e}")
     else:
-        SKLog.error("read_input_registers, client is not connected")   
+        SKLog.error("read_input_registers, client is not connected")
