e1db4ca0f7bfae7c046b8551e14fb218f76f3007
Yongle <yong.work@gmail.com>
2025-04-24 09:46:32 +0800

    feat: update launch file to support multiple parameter files

--- ros2_stark_ws/src/ros2_stark_controller/launch/stark_launch.py
+++ ros2_stark_ws/src/ros2_stark_controller/launch/stark_launch.py
@@ -10,12 +10,25 @@ def generate_launch_description():
             default_value='path/to/ros2_stark_ws/src/ros2_stark_controller/config/params.yaml', # Update with the correct path
             description='Path to the ROS2 parameters file'
         ),
+        # 多只手
+        # DeclareLaunchArgument(
+        #     'params_file_2',
+        #     default_value='path/to/ros2_stark_ws/src/ros2_stark_controller/config/params_2.yaml', # Update with the correct path
+        #     description='Path to the ROS2 parameters file'
+        # ),
         
         Node(
             package='ros2_stark_controller',
             executable='stark_node',
-            name='stark_node',
+            name='stark_node_1',
             output='screen',
             parameters=[LaunchConfiguration('params_file')]
         ),
+        # Node(
+        #     package='ros2_stark_controller',
+        #     executable='stark_node',
+        #     name='stark_node_2',
+        #     output='screen',
+        #     parameters=[LaunchConfiguration('params_file_2')]
+        # ),
     ])
