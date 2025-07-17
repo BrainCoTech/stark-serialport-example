1b6f3b8e16505ced7515ca35148c22b96fd91313
Yongle <yong.work@gmail.com>
2025-07-17 19:46:07 +0800

    bump: update bc-stark-sdk version to v0.6.8

--- python/revo2/revo2_dfu.py
+++ python/revo2/revo2_dfu.py
@@ -3,6 +3,7 @@ import sys
 import pathlib
 import os
 import time
+
 from utils import setup_shutdown_event
 from revo2_utils import libstark, logger
 
@@ -27,7 +28,7 @@ main_loop = None
 def on_dfu_state(_slave_id, state):
     logger.info(f"DFU STATE: {libstark.DfuState(state)}")
     dfu_state = libstark.DfuState(state)
-    if state == libstark.DfuState.Completed or dfu_state == libstark.DfuState.Aborted:
+    if dfu_state == libstark.DfuState.Completed or dfu_state == libstark.DfuState.Aborted:
         if main_loop and shutdown_event:
             if not shutdown_event.is_set():
                 logger.info("Using call_soon_threadsafe to set event")
@@ -38,43 +39,36 @@ def on_dfu_state(_slave_id, state):
 def on_dfu_progress(_slave_id, progress):
     logger.info(f"progress: {progress * 100.0 :.2f}%")
 
-
 # Main
 async def main():
-    global shutdown_event, main_loop
+    global  main_loop
     main_loop = asyncio.get_running_loop()
-    shutdown_event = setup_shutdown_event(logger)
 
     (protocol, port_name, baudrate, slave_id) = await libstark.auto_detect_device()
-    logger.info(
-        f"Detected protocol: {protocol}, port: {port_name}, baudrate: {baudrate}, slave_id: {slave_id}"
-    )
-    client = await libstark.modbus_open(port_name, baudrate)
+    logger.info(f"Detected protocol: {protocol}, port: {port_name}, baudrate: {baudrate}, slave_id: {slave_id}")
 
-    ota_bin_path = ""
-    # 重要！！！：不同硬件需使用相应固件，否则需要拆设备重新烧录
-    if protocol == libstark.StarkProtocolType.Modbus:
-        device_info: libstark.DeviceInfo = await client.get_device_info(slave_id)
-        logger.info(f"Device info: {device_info.description}")
-        if device_info.is_revo2():
-            # ota_bin_path = revo2_basic_ota_bin_path
-            if device_info.is_revo2_touch():
-                logger.error("Revo2 Touch firmware is not supported in this script.")
-                sys.exit(1)
-            else:
-                ota_bin_path = revo2_basic_ota_bin_path
-    else:
-        logger.error(f"Unsupported protocol: {protocol}")
-        sys.exit(1)
-
+    ota_bin_path = revo2_basic_ota_bin_path
     if not os.path.exists(ota_bin_path):
         logger.warning(f"OTA文件不存在: {ota_bin_path}")
         exit(0)
     else:
         logger.info(f"OTA文件路径: {ota_bin_path}")
 
-    # 固件升级
-    logger.info("start_dfu")
+    await start_dfu(port_name, baudrate, slave_id, ota_bin_path)
+    # 测试再次升级
+    # await asyncio.sleep(5)
+    # await start_dfu(port_name, baudrate, slave_id, ota_bin_path)
+
+    sys.exit(0)
+
+async def start_dfu(port_name, baudrate, slave_id, ota_bin_path):
+    global shutdown_event
+    shutdown_event = setup_shutdown_event(logger)
+
+    client = await libstark.modbus_open(port_name, baudrate)
+    device_info: libstark.DeviceInfo = await client.get_device_info(slave_id)
+    logger.info(f"Device info: {device_info.description}")
+
     wait_seconds = 5  # wait device enter DFU mode
     await client.start_dfu(
         slave_id,
@@ -85,13 +79,12 @@ async def main():
     )
 
     # 等待关闭事件
+    logger.info("Waiting for DFU to complete...")
     await shutdown_event.wait()
-
-    # 关闭资源
+    logger.info("DFU completed, shutdown event received!")
+    logger.info("start modbus close")
     libstark.modbus_close(client)
     logger.info("Modbus client closed")
-    sys.exit(0)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
